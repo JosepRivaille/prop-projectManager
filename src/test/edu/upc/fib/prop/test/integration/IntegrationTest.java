0537804253497c9649b718d2cd25e6b46527cb26
JosepRIvaille <josep.de.cid@gmail.com>
2016-09-21 19:05:57 +0200

    Create project structure

---
+++ src/test/edu/upc/fib/prop/test/integration/IntegrationTest.java
@@ -0,0 +1,15 @@
+package edu.upc.fib.prop.test.integration;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class IntegrationTest {
+
+    @Test
+    public void whenTestIfJunitIntegrationWorks_thenTestShouldPass() {
+        assertTrue(true);
+    }
+
+}
---
+++ src/test/edu/upc/fib/prop/test/unit/UnitTest.java
@@ -0,0 +1,14 @@
+package edu.upc.fib.prop.test.unit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UnitTest {
+
+    @Test
+    public void whenTestIfJunitWorks_thenTestShouldPass() {
+        assertTrue(true);
+    }
+
+}
