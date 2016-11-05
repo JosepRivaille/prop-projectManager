fb2cda1ae456bc2698d79b4cffd88cd641730cba
JosepRivaille <josep.de.cid@gmail.com>
2016-11-05 12:29:44 +0100

    Create Utils and exception for authors

---
+++ src/main/java/edu/upc/fib/prop/exceptions/AuthorNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.upc.fib.prop.exceptions;
+
+/**
+ * Created by joseprivaille on 5/11/16.
+ */
+public class AuthorNotFoundException {
+}
---
+++ src/main/java/edu/upc/fib/prop/utils/IOUtils.java
@@ -0,0 +1,7 @@
+package edu.upc.fib.prop.utils;
+
+/**
+ * Created by joseprivaille on 5/11/16.
+ */
+public class IOUtils {
+}
