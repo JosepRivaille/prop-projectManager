ae36f11b01f6b30d03ea4dda9dd327c680d7b38d
Unknown <guillermovalentin14@gmail.com>
2016-12-03 13:33:46 +0100

    document-info (con errores) 2

--- src/main/resources/view/components/document-info/document-info.js
+++ src/main/resources/view/components/document-info/document-info.js
@@ -1,3 +1,30 @@
-/**
- * Created by Guillermo on 03/12/2016.
- */
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc component
+     * @name project.components.component:DocumentInfo
+     *
+     * @description
+     *
+     */
+    angular
+        .module('project')
+        .controller('DocumentInfoCtrl', DocumentInfoCtrl)
+        .component('documentInfo', {
+            templateUrl: 'components/document-info/document-info.tpl.html',
+            controller: DocumentInfoCtrl
+        });
+
+    function DocumentInfoCtrl($stateParams) {
+        var vm = this;
+        vm.ctrlName = 'DocumentInfoCtrl';
+
+        vm.title = "TITLE_DOCUMENT_INFO";
+
+        vm.document = $stateParams.documentSelected;
+
+        alert(123);
+    }
+
+}());
