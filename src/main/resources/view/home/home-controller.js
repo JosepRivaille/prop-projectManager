bac6c8fdf19ce282d4ca21ff30ddd5eb6e0b4598
Guillermo <guillermovalentin14@gmail.com>
2016-12-10 10:34:12 +0100

    asfa

--- src/main/resources/view/home/home-controller.js
+++ src/main/resources/view/home/home-controller.js
@@ -15,7 +15,20 @@
     function HomeCtrl($rootScope) {
         var vm = this;
         vm.ctrlName = 'HomeCtrl';
+        vm.isDocumentSelected = false;
         vm.recommendedDocs = JSON.parse($rootScope.backendService.getRecommendedDocs(5));
         vm.visitedDocs = JSON.parse($rootScope.backendService.getVisitedDocs(7));
+
+        vm.select = function(document){
+            vm.isDocumentSelected = true;
+            vm.selectedDocument = document;
+        }
+
+        vm.back = function(){
+            vm.isDocumentSelected = false;
+            vm.documentSelected = undefined;
+        }
     }
+
+
 }());
